1e7b720e9251c2db96fdb76fb862f4e5d3199c3f
eman obeidat <amoonobeidat43@gmail.com>
2023-08-23 20:23:07 +0000

    lab 39

--- cookie-stand-admin/pages/overview.js
+++ cookie-stand-admin/pages/overview.js
@@ -7,7 +7,7 @@ export default function Overview() {
         <>
             <Hhead data={"Overview"} />
             <Header nav={"Home"} lin={'/'} />
-            <h1 className='mt-5 text-3xl font-bold leading-9 tracking-tight text-center text-gray-900' >Hello In Overview Page</h1>
+            <h1 className='mt-5 text-3xl font-bold leading-9 tracking-tight text-center text-gray-900' >This is Overview Page</h1>
             <Footer data={""}/>
         </>
     )
